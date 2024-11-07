7cc601405abcc174dc4a1d682186d6ab174ff5bd
Your Name <Mohit.Kale@adixoo.com>
2024-11-07 07:25:02 +0530

    Initial Carousel Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Carousel from './components/Carousel'
 const page = () => {
   return (
     <div>
       <Navbar/>
       <main className='h-screen'>
-
+        <Carousel/>
       </main>
       <Footer/>
     </div>
