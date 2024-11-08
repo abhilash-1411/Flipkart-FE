781addbba832839b6a5af84eb1ccb9089d756de4
Your Name <Mohit.Kale@adixoo.com>
2024-11-07 16:42:45 -0800

    Initial Footer Setup

---
+++ app/components/Footer.tsx
@@ -0,0 +1,13 @@
+// components/Footer.tsx
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="bg-gray-900 text-white py-12">
+      <div className="max-w-screen-xl mx-auto px-4">
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 const page = () => {
   return (
     <div>
       <Navbar/>
+      <main className='h-screen'>
+
+      </main>
+      <Footer/>
     </div>
   )
 }
