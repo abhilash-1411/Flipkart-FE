93b52598c052062e1aa2f4ea196f37da88ba1476
unknown <abhilash.chandravanshi@adixoo.com>
2024-11-11 11:16:01 +0530

    signup API integrated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import BannerComponent from "./BannerComponent/page";
-// import CardComponent from "./CardComponent/page"
-// import CardComponent from "./CardComponent/page"
+
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -31,9 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
-        <BannerComponent/>
-        {/* <CardComponent/> */}
-        {/* <CardComponent/> */}
+   
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import BannerComponent from "./BannerComponent/page";
 const page = () => {
   return (
     <div>
       <Navbar/>
+      <BannerComponent/>
     </div>
   )
 }
